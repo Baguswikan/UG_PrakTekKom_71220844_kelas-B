6d0c8018399a6b1c3929072175c2b115532df365
Baguswikan <117887575+Baguswikan@users.noreply.github.com>
2022-12-01 12:59:00 +0700

    Add files via upload

--- UG_11_71220844/soal_2.py
+++ UG_11_71220844/soal_2.py
@@ -1,8 +1,8 @@
-x = input("masukan kata")
+x = input("masukan kata: ")
 palindrome = True
-panjang_y = len(x)
-for i in range(panjang_y//2):
-    if (x[i]) != x([panjang_y-i-1]):
+panjang_x = len(x)
+for i in range(panjang_x//2):
+    if (x[i] != x[panjang_x-i-1]):
         palindrome = False
         break
 if palindrome:
